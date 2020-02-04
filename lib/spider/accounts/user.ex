8c690f85046e3e967fd028f84a217d515a45bfa4
= <kelraf11746@gmail.com>
2020-02-04 10:27:03 +0300

    user creation

--- lib/spider/accounts/user.ex
+++ lib/spider/accounts/user.ex
@@ -25,6 +25,8 @@ defmodule Spider.Accounts.User do
     user
     |> cast(attrs, [:phone_number, :email, :raw_password, :password_hash, :verification_code, :first_name, :last_name, :role, :national_id_number, :kra_pin])
     |> validate_required([:phone_number, :raw_password, :first_name, :last_name, :role])
+    |> validate_length(:raw_password, min: 6, max: 15)
+    |> validate_length(:phone_number, min: 10, max: 15)
     |> UserToolKit.validate_roles()
     |> UserToolKit.password_hash()
     |> UserToolKit.set_verification_code()
