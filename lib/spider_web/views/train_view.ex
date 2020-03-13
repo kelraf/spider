6bcc12b06fe6375d845e83d537b22872262a372f
= <kelraf11746@gmail.com>
2020-03-13 13:51:36 +0300

    cleaned trainc view and created train view

---
+++ lib/spider_web/views/train_view.ex
@@ -0,0 +1,22 @@
+defmodule SpiderWeb.TrainView do
+  use SpiderWeb, :view
+  alias SpiderWeb.TrainView
+
+  def render("index.json", %{trains: trains}) do
+    %{data: render_many(trains, TrainView, "train.json")}
+  end
+
+  def render("show.json", %{train: train}) do
+    %{data: render_one(train, TrainView, "train.json")}
+  end
+
+  def render("train.json", %{train: train}) do
+    %{
+      id: train.id,
+      unique_number: train.unique_number,
+      role: train.role,
+      business_id: train.business_id,
+      user_id: train.user_id
+    }
+  end
+end
---
+++ priv/repo/migrations/20200313104800_create_trains.exs
@@ -0,0 +1,14 @@
+defmodule Spider.Repo.Migrations.CreateTrains do
+  use Ecto.Migration
+
+  def change do
+    create table(:trains) do
+      add :unique_number, :string
+      add :role, :string
+
+      timestamps()
+    end
+
+    create unique_index(:trains, [:unique_number])
+  end
+end
